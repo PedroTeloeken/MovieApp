6ef6ab2fb59c7cd6e47012a0a49f187acea1852b
pedro.teloeken <pedro.teloeken@joinin.com.br>
2024-01-05 15:51:05 -0300

    Criando o Interceptor para passar a key e a language

---
+++ app/src/main/java/br/com/movieapp/core/util/Constants.kt
@@ -0,0 +1,9 @@
+package br.com.movieapp.core.util
+
+object Constants {
+
+    const val API_KEY_PARAM = "api_key"
+    const val LANGUAGE_PARAM = "language"
+    const val LANGUAGE_VALUE = "pt-BR"
+
+}
