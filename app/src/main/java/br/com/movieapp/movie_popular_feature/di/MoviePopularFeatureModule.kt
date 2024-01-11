e8cdfede81e9dc9563104e58ca3b5f5b50c6961c
Pedro Teloeken <pedro.teloeken@joinin.com.br>
2024-01-11 08:23:18 -0300

    Criando o di

---
+++ app/src/main/java/br/com/movieapp/movie_popular_feature/di/MoviePopularFeatureModule.kt
@@ -0,0 +1,29 @@
+package br.com.movieapp.movie_popular_feature.di
+
+import br.com.movieapp.core.data.remote.service.MovieService
+import br.com.movieapp.movie_popular_feature.data.repository.MoviePopularRepositoryImpl
+import br.com.movieapp.movie_popular_feature.data.source.MoviePopularRemoteDataSourceImpl
+import br.com.movieapp.movie_popular_feature.domain.repository.MoviePopularRepository
+import br.com.movieapp.movie_popular_feature.domain.source.MoviePopularRemoteDataSource
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SinceKotlin::class)
+object MoviePopularFeatureModule {
+
+    @Singleton
+    @Provides
+    fun providesDataSource(service: MovieService): MoviePopularRemoteDataSource {
+        return MoviePopularRemoteDataSourceImpl(service = service)
+    }
+
+    @Singleton
+    @Provides
+    fun provideMovieRepository(remoteDataSource: MoviePopularRemoteDataSource): MoviePopularRepository {
+        return MoviePopularRepositoryImpl(remoteDataSource = remoteDataSource)
+    }
+
+}
