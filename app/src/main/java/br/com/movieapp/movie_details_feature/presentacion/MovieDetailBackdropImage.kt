19fe3727a3f73c8d818cda8981652f1b75fd06ae
Pedro Teloeken <pedro.teloeken@joinin.com.br>
2024-01-14 15:30:10 -0300

    Criando o MovieDetailBackdropImage

---
+++ app/src/main/java/br/com/movieapp/movie_details_feature/presentacion/MovieDetailBackdropImage.kt
@@ -0,0 +1,47 @@
+package br.com.movieapp.movie_details_feature.presentacion
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import br.com.movieapp.R
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+
+@Composable
+fun MovieDetailBackdropImage(
+    modifier: Modifier = Modifier,
+    imageUrl: String,
+) {
+
+    Box(modifier = modifier) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(imageUrl)
+                .crossfade(true)
+                .error(R.drawable.ic_error_image)
+                .placeholder(R.drawable.ic_placeholder)
+                .build(),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = modifier
+        )
+    }
+
+}
+
+@Preview
+@Composable
+private fun MovieDetailBackdropImagePreview() {
+    MovieDetailBackdropImage(
+        imageUrl = "",
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(200.dp)
+    )
+}
