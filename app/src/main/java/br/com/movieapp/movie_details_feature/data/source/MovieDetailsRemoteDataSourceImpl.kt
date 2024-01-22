d855aca7429e0f2579943766ee9e5f3dcc86a202
pedro.teloeken <pedro.teloeken@joinin.com.br>
2024-01-22 08:39:48 -0300

    Criando teste para o Paging de movies similar

--- app/src/main/java/br/com/movieapp/movie_details_feature/data/source/MovieDetailsRemoteDataSourceImpl.kt
+++ app/src/main/java/br/com/movieapp/movie_details_feature/data/source/MovieDetailsRemoteDataSourceImpl.kt
@@ -1,15 +1,16 @@
 package br.com.movieapp.movie_details_feature.data.source
 
-import br.com.movieapp.core.data.remote.response.MovieResponse
 import br.com.movieapp.core.data.remote.service.MovieService
 import br.com.movieapp.core.domain.MovieDetails
+import br.com.movieapp.core.domain.MoviePaging
 import br.com.movieapp.core.paging.MovieSimilarPagingSource
 import br.com.movieapp.core.util.toBackDropUrl
 import br.com.movieapp.movie_details_feature.domain.source.MovieDetailsRemoteDataSource
+import br.com.movieapp.movie_popular_feature.data.mapper.toMovie
 import javax.inject.Inject
 
 class MovieDetailsRemoteDataSourceImpl @Inject constructor(
-private val service: MovieService
+    private val service: MovieService
 ) : MovieDetailsRemoteDataSource {
     override suspend fun getMovieDetails(movieId: Int): MovieDetails {
         val response = service.getMovie(movieId = movieId)
@@ -18,7 +19,7 @@ private val service: MovieService
             id = response.id,
             title = response.title,
             overview = response.overview,
-            genres =  response.genres.map { it.name },
+            genres = response.genres.map { it.name },
             releaseDate = response.releaseDate,
             backdropPathUrl = response.backdropPath.toBackDropUrl(),
             voteAverage = response.voteAverage,
@@ -28,11 +29,18 @@ private val service: MovieService
 
     }
 
-    override suspend fun getMoviesSimilar(page: Int, movieId: Int): MovieResponse {
-        return service.getMoviesSimilar(page = page , movieId = movieId)
+    override suspend fun getMoviesSimilar(page: Int, movieId: Int): MoviePaging {
+        val response = service.getMoviesSimilar(page = page, movieId = movieId)
+
+        return MoviePaging(
+            page = response.page,
+            totalPage = response.totalPages,
+            totalResults = response.totalResults,
+            movies = response.results.map { it.toMovie() }
+        )
     }
 
     override fun getSimilarMoviePagingSource(movieId: Int): MovieSimilarPagingSource {
-       return MovieSimilarPagingSource(this , movieId = movieId)
+        return MovieSimilarPagingSource(this, movieId = movieId)
     }
 }
--- app/src/main/java/br/com/movieapp/movie_details_feature/domain/source/MovieDetailsRemoteDataSource.kt
+++ app/src/main/java/br/com/movieapp/movie_details_feature/domain/source/MovieDetailsRemoteDataSource.kt
@@ -1,14 +1,14 @@
 package br.com.movieapp.movie_details_feature.domain.source
 
-import br.com.movieapp.core.data.remote.response.MovieResponse
 import br.com.movieapp.core.domain.MovieDetails
+import br.com.movieapp.core.domain.MoviePaging
 import br.com.movieapp.core.paging.MovieSimilarPagingSource
 
 interface MovieDetailsRemoteDataSource {
 
     suspend fun getMovieDetails(movieId: Int): MovieDetails
 
-    suspend fun getMoviesSimilar(page: Int, movieId: Int) : MovieResponse
+    suspend fun getMoviesSimilar(page: Int, movieId: Int) : MoviePaging
 
     fun getSimilarMoviePagingSource(movieId: Int) : MovieSimilarPagingSource
 
